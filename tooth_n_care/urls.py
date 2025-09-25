b3db36f71a08f0e5353e5f6ec47fed4e982f34d1
Guilherme Fusuma <fuskenji@gmail.com>
2025-09-25 16:36:02 -0300

    apps prontos para montar views

--- tooth_n_care/urls.py
+++ tooth_n_care/urls.py
@@ -16,15 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-import medicos
-import consultas
-import pacientes
-import appadmin
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('medico/', include(medicos.urls)),
-    path('consulta/', include(consultas.urls)),
-    path('paciente/', include(pacientes.urls)),
-    path('appadmin/', include(appadmin.urls)),
+    path('medico/', include('medicos.urls')),
+    path('consulta/', include('consultas.urls')),
+    path('paciente/', include('pacientes.urls')),
+    path('appadmin/', include('appadmin.urls')),
 ]
