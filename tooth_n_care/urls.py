da5a12ac104c15092e4de2eaedb790bf1f1afdf6
Guilherme Fusuma <fuskenji@gmail.com>
2025-09-25 14:05:44 -0300

    criado apps

--- tooth_n_care/urls.py
+++ tooth_n_care/urls.py
@@ -15,8 +15,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+import medicos
+import consultas
+import pacientes
+import appadmin
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('medico/', include(medicos.urls)),
+    path('consulta/', include(consultas.urls)),
+    path('paciente/', include(pacientes.urls)),
+    path('appadmin/', include(appadmin.urls)),
 ]
