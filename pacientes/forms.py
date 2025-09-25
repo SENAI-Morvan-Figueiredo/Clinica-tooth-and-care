a8230e47092a178b95975bf39eba6f7a804b6ba0
Guilherme Fusuma <fuskenji@gmail.com>
2025-09-25 16:36:02 -0300

    apps prontos para montar views

--- pacientes/forms.py
+++ pacientes/forms.py
@@ -1,7 +1,21 @@
 from django.forms import forms
 from .models import Paciente
 
-class pacienteForm(forms.ModelForm):
+class PacienteForm(forms.ModelForm):
     class Meta:
         model = Paciente
-        fields = ['nome', 'cpf', 'rg', 'email', 'telefone', 'data_nasc', 'genero', 'endereco']
+        fields = [
+            "nome",
+            "cpf",
+            "rg",
+            "email",
+            "telefone",
+            "data_nasc",
+            "genero",
+            "endereco",
+        ]
+        
+        widgets = {
+            "data_nasc": forms.DateInput(attrs={"type": "date"}),  # input tipo calendário
+            "genero": forms.Select(),  # já usa GENREO_CHOICES do model
+        }
