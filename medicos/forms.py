493d8789dc1bd7ec4f4efee2b83a75ab86685971
Victor <victorantonio.vats1@gmail.com>
2025-09-25 16:19:08 -0300

    widgets_forms

--- medicos/forms.py
+++ medicos/forms.py
@@ -13,3 +13,32 @@ class MedicoForm(forms.ModelForm):
             "telefone",
             "especialidades",
         ]
+        
+        widgets = {
+            "nome": forms.TextInput(attrs={
+                "class": "form-control",
+                "placeholder": "Nome completo"
+            }),
+            "email": forms.EmailInput(attrs={
+                "class": "form-control",
+                "placeholder": "exemplo@dominio.com"
+            }),
+            "cpf": forms.TextInput(attrs={
+                "class": "form-control",
+                "placeholder": "000.000.000-00"
+            }),
+            "rg": forms.TextInput(attrs={
+                "class": "form-control",
+                "placeholder": "Número do RG"
+            }),
+            "crm": forms.TextInput(attrs={
+                "class": "form-control",
+                "placeholder": "Número do CRM"
+            }),
+            "telefone": forms.TextInput(attrs={
+                "class": "form-control",
+                "placeholder": "(99) 99999-9999"
+            }),
+            "especialidades": forms.CheckboxSelectMultiple(),  
+            # ou forms.SelectMultiple(attrs={"class": "form-control"}) para <select multiple>
+        }
