7d2e7e53b880665e930ef8bfbbfde4b4f85997c6
Victor <victorantonio.vats1@gmail.com>
2025-09-25 14:51:02 -0300

    model medicos

--- medicos/models.py
+++ medicos/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+class Especialidade(models.Model):
+    nome = models.CharField(max_length=255, null=False, blank=False)
+
+    def __str__(self):
+        return self.nome
+
+class Medico(models.Model):
+    nome = models.CharField(max_length=255, null=False, blank=False)
+    email = models.EmailField(max_length=255, null=False, blank=False, unique=True)
+    cpf = models.CharField(max_length=14, null=False, blank=False, unique=True)  # 000.000.000-00
+    rg = models.CharField(max_length=20, null=False, blank=False, unique=True)
+    crm = models.CharField(max_length=20, null=False, blank=False, unique=True)
+    telefone = models.CharField(max_length=20, null=False, blank=False)
+
+    especialidades = models.ManyToManyField(
+        Especialidade,
+        related_name="medicos"  # permite acessar os médicos a partir da especialidade
+    )
+
+    def __str__(self):
+        return self.nome
+    
+
+
