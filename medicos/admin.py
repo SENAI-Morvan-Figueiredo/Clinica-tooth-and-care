8d47f6b7ff3c90ff14f382b98873051ad6a33133
Guilherme Fusuma <fuskenji@gmail.com>
2025-09-26 15:36:21 -0300

    Feito página de admin do django

--- medicos/admin.py
+++ medicos/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Medico, Especialidade
 
 # Register your models here.
+@admin.register(Medico)
+class MedicoAdmin(admin.ModelAdmin):
+    list_display = ['nome', 'email', 'cpf', 'rg', 'crm', 'telefone']
+    search_fields = ['nome', 'email', 'cpf', 'rg', 'crm']
+
+    fieldsets = (
+        ('Informações pessoais', {
+            'fields': ('nome', 'cpf', 'rg', 'crm')
+        }),
+        (('Contato'), {
+            'fields': ('email', 'telefone')
+        })
+    )
+
+@admin.register(Especialidade)
+class EspecialidadeAdmin(admin.ModelAdmin):
+    list_display = ['nome']
+    search_fields = ['nome']
