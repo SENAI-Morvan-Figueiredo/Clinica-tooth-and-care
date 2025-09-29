d8eeef648cf6ca2189579033bc4e7a1767946e8d
Guilherme Fusuma <fuskenji@gmail.com>
2025-09-29 16:53:12 -0300

    dashboard do admin funcional, mas não estilizado

--- medicos/admin.py
+++ medicos/admin.py
@@ -4,7 +4,7 @@ from .models import Medico, Especialidade
 # Register your models here.
 @admin.register(Medico)
 class MedicoAdmin(admin.ModelAdmin):
-    list_display = ['nome', 'email', 'cpf', 'rg', 'crm', 'telefone']
+    list_display = ['nome', 'email', 'cpf', 'rg', 'crm', 'telefone', 'get_especialidades']
     search_fields = ['nome', 'email', 'cpf', 'rg', 'crm']
 
     fieldsets = (
@@ -13,8 +13,16 @@ class MedicoAdmin(admin.ModelAdmin):
         }),
         (('Contato'), {
             'fields': ('email', 'telefone')
+        }),
+        (('Outros'), {
+            'fields': ('especialidades',)
         })
     )
+
+    def get_especialidades(self, obj):
+        return ", ".join([esp.nome for esp in obj.especialidades.all()])
+
+    get_especialidades.short_description = "Especialidades"
 
 @admin.register(Especialidade)
 class EspecialidadeAdmin(admin.ModelAdmin):
