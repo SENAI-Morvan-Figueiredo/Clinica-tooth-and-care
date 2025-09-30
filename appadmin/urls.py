0caf6a2b449f2f29c0098816f7ea837f644e711c
Guilherme Fusuma <fuskenji@gmail.com>
2025-09-30 16:26:55 -0300

    Criado telas iniciais do adm e modificado pacientes e medicos para usar User do django no models

--- appadmin/urls.py
+++ appadmin/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('dashboard/', views.dashboard, name='dashboard'),
+    path('admusuarios/', views.pacientes, name="adm-pacientes"),
+    path('admmedicos/', views.medicos, name="adm-medicos"),
+    path('admconsultas/', views.consultas, name="adm-consultas"),
 ]
--- medicos/admin.py
+++ medicos/admin.py
@@ -4,20 +4,8 @@ from .models import Medico, Especialidade
 # Register your models here.
 @admin.register(Medico)
 class MedicoAdmin(admin.ModelAdmin):
-    list_display = ['nome', 'email', 'cpf', 'rg', 'crm', 'telefone', 'get_especialidades']
-    search_fields = ['nome', 'email', 'cpf', 'rg', 'crm']
-
-    fieldsets = (
-        ('Informações pessoais', {
-            'fields': ('nome', 'cpf', 'rg', 'crm')
-        }),
-        (('Contato'), {
-            'fields': ('email', 'telefone')
-        }),
-        (('Outros'), {
-            'fields': ('especialidades',)
-        })
-    )
+    list_display = ['user__username', 'user__email', 'cpf', 'rg', 'crm', 'telefone', 'get_especialidades']
+    search_fields = ['user__username', 'user__email', 'cpf', 'rg', 'crm']
 
     def get_especialidades(self, obj):
         return ", ".join([esp.nome for esp in obj.especialidades.all()])
@@ -28,3 +16,18 @@ class MedicoAdmin(admin.ModelAdmin):
 class EspecialidadeAdmin(admin.ModelAdmin):
     list_display = ['nome']
     search_fields = ['nome']
+
+class MedicoInline(admin.StackedInline):
+    model = Medico
+    can_delete = False
+    verbose_name_plural = 'Perfil de Médico'
+    
+    # Define a ordem e agrupamento dos campos do Médico
+    fieldsets = (
+        ('Detalhes Profissionais', {
+            'fields': ('crm', 'especialidades', 'telefone')
+        }),
+        ('Documentos', {
+            'fields': ('cpf', 'rg')
+        }),
+    )
