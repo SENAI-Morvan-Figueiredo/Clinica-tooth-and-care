dd4bb79db2d68596864c2389e137bdb0e5cd8636
Guilherme Fusuma <fuskenji@gmail.com>
2025-09-30 16:26:55 -0300

    Criado telas iniciais do adm e modificado pacientes e medicos para usar User do django no models

--- appadmin/views.py
+++ appadmin/views.py
@@ -3,6 +3,7 @@ from pacientes.models import Paciente
 from medicos.models import Medico, Especialidade
 from consultas.models import Consulta
 from django.utils import timezone
+from datetime import datetime, timedelta, time
 
 # Create your views here.
 def dashboard(request):
@@ -21,3 +22,46 @@ def dashboard(request):
         }
         return render(request, "appadmin/dashboard.html", context) 
     
+def pacientes(request):
+    if request.method == 'GET':
+        pacientes = Paciente.objects.all()
+
+        return render(request, "appadmin/admPacientes.html", {"pacientes": pacientes})
+
+def medicos(request):
+    if request.method == 'GET':
+        medicos = Medico.objects.all()
+
+        return render(request, "appadmin/admMedicos.html", {"medicos": medicos})
+    
+def gerar_slots_de_tempo(hora_inicio, hora_fim, intervalo_minutos):
+    # Combina a hora com a data de hoje para criar objetos datetime iteráveis
+    data_hoje = datetime.today().date()
+    inicio_dt = datetime.combine(data_hoje, hora_inicio)
+    fim_dt = datetime.combine(data_hoje, hora_fim)
+    
+    slots = []
+    tempo_atual = inicio_dt
+    
+    # Itera enquanto o tempo atual for menor ou igual ao tempo final
+    while tempo_atual <= fim_dt:
+        # Adiciona a hora formatada à lista
+        slots.append(tempo_atual.strftime('%H:%M'))
+        
+        # Avança para o próximo slot
+        tempo_atual += timedelta(minutes=intervalo_minutos)
+        
+    return slots
+
+def consultas(request):
+    if request.method == 'GET':
+        consultas = Consulta.objects.all()
+        horarios = gerar_slots_de_tempo(time(6, 0), time(20, 0), 15) # consegue todos os horários de consultas
+
+        context = {
+            "consultas": consultas, 
+            "horarios": horarios,
+            "servicos": Consulta.SERVICOS
+            }
+
+        return render(request, 'appadmin/admConsultas.html', context)
--- pacientes/admin.py
+++ pacientes/admin.py
@@ -2,20 +2,20 @@ from django.contrib import admin
 from .models import Paciente
 
 # Register your models here.
+class PacienteInline(admin.StackedInline):
+    model = Paciente
+    can_delete = False
+    verbose_name_plural = 'Informações do Paciente'
+    
+    # Dentro do Inline, você pode organizar os campos do Paciente:
+    fieldsets = (
+        ('Detalhes do Paciente', {
+            'fields': ('cpf', 'rg', 'telefone', 'data_nasc', 'genero', 'endereco')
+        }),
+    )
+
 @admin.register(Paciente)
 class PacienteAdmin(admin.ModelAdmin):
-    list_display = ['nome', 'cpf', 'rg', 'email', 'telefone', 'data_nasc', 'genero', 'endereco']
+    list_display = ['user__username', 'cpf', 'rg', 'data_nasc', 'user__email']
     list_filter = ['genero']
-    search_fields = ['nome', 'cpf', 'rg', 'email']
-
-    fieldsets = (
-        ('Informações pessoais', {
-            'fields': ('nome', 'cpf', 'rg', 'data_nasc', 'genero')
-        }),
-        ('Contato', {
-            'fields': ('email', 'telefone')
-        }),
-        ('Endereço', {
-            'fields': ('endereco',)
-        })
-    )
+    search_fields = ['cpf', 'rg', 'user__username', 'user__last_name']
