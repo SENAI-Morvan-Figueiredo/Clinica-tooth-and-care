e1565239d63d322253470b1c1aceb267384933ac
marcio felipe <cmarciofelipe7@gmail.com>
2025-10-14 13:55:22 -0300

    crud

--- consultas/models.py
+++ consultas/models.py
@@ -5,11 +5,11 @@ from medicos.models import Medico
 class Consulta(models.Model):
     data = models.DateTimeField(null=False,blank=False)
     sala = models.CharField(max_length=50,null=False,blank=False)
-    servico =  models.CharField(max_length=100,choices=[],null=False,blank=False)
+    servico =  models.CharField(max_length=100,choices=[("clinico geral","Clinico geral")],null=False,blank=False)
     valor = models.DecimalField(max_digits=8, decimal_places=2,null=False,blank=False)
     status =  models.CharField(max_length=20,choices=[("marcada","Marcada"),("realizada","Realizada"),("cancelada","Cancelada"),("remarcada","Remarcada")],null=False,blank=False)
-    paciente = models.ForeignKey(Paciente, on_delete=models.CASCADE, related_name="consultas")
-    medico = models.ForeignKey(Medico, on_delete=models.CASCADE, related_name="consultas")
+    paciente = models.ForeignKey(Paciente, on_delete=models.CASCADE,related_name="consultas")
+    medico = models.ForeignKey(Medico, on_delete=models.CASCADE,related_name="consultas")
     def __str__(self):
         return f"Consulta de {self.paciente} em {self.data.strftime('%d/%m/%Y %H:%M')}"
 
