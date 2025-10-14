a6112fa37c96f53e841908bdabaf8ce7d6a8a91b
marcio felipe <cmarciofelipe7@gmail.com>
2025-10-14 13:55:22 -0300

    crud

--- pacientes/views.py
+++ pacientes/views.py
@@ -5,17 +5,17 @@ from consultas.forms import ConsultaForm
 
 
 def consulta_lista(request):
-    """
-    Lista todas as consultas.
-    """
     consultas = Consulta.objects.all().order_by("-data")
-    return render(request, "paciente/consulta/crud.html", {"consultas": consultas})
+    form = ConsultaForm()
+    return render(request, "paciente/consulta/crud.html", {
+        "consultas": consultas,
+        "form": form
+    })
 
 
 def consulta_criar(request):
-    """
-    Cria uma nova consulta.
-    """
+    consultas = Consulta.objects.all().order_by("-data")
+
     if request.method == "POST":
         form = ConsultaForm(request.POST)
         if form.is_valid():
@@ -24,14 +24,17 @@ def consulta_criar(request):
             return redirect("consultas-lista")
     else:
         form = ConsultaForm()
-    return render(request, "paciente/consulta/crud.html", {"form": form})
+
+    return render(request, "paciente/consulta/crud.html", {
+        "form": form,
+        "consultas": consultas
+    })
 
 
 def consulta_editar(request, consulta_id):
-    """
-    Edita uma consulta existente.
-    """
     consulta = get_object_or_404(Consulta, id=consulta_id)
+    consultas = Consulta.objects.all().order_by("-data")
+
     if request.method == "POST":
         form = ConsultaForm(request.POST, instance=consulta)
         if form.is_valid():
@@ -40,13 +43,16 @@ def consulta_editar(request, consulta_id):
             return redirect("consultas-lista")
     else:
         form = ConsultaForm(instance=consulta)
-    return render(request, "paciente/consulta/crud.html", {"form": form, "consulta": consulta})
+
+    return render(request, "paciente/consulta/crud.html", {
+        "form": form,
+        "consultas": consultas,
+        "consulta": consulta,
+        "consulta_editando": True
+    })
 
 
 def consulta_excluir(request, consulta_id):
-    """
-    Exclui uma consulta existente.
-    """
     consulta = get_object_or_404(Consulta, id=consulta_id)
     consulta.delete()
     messages.success(request, f"Consulta ID {consulta_id} excluída com sucesso!")
