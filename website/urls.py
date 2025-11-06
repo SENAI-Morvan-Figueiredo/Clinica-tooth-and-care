d35ec0c75db380b6c693104bfa61beed1eada179
Guilherme Fusuma <fuskenji@gmail.com>
2025-11-06 14:46:53 -0300

    ajustes

--- website/urls.py
+++ website/urls.py
@@ -3,9 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='index'),
-<<<<<<< HEAD
-=======
     path('login', views.login_view, name="custom-login"),
->>>>>>> login
     path('get_user_type', views.get_user_type, name='get_user_type'),
 ]
